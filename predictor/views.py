9bc5d144b192152c4fe125e806d30877d4d1d1fd
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2017-10-15 09:19:28 +0530

    Create app predictor and HomeView

---
+++ predictor/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class HomeView(View):
+	def get(self, request, *args, **kwargs):
+		context = {}
+		return render(request, 'home.html', context)
